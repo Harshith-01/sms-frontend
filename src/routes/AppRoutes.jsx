d56983152d17f5181ea72a4cbf8a8509cfb781b4
Ravitheja <ravithejaravi14@gmail.com>
2026-01-30 11:02:42 +0530

    Added Landin age and Authentication pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,9 +4,7 @@ import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
 import ForgotPassword from '../pages/auth/ForgotPassword';
 import ResetPassword from '../pages/auth/ResetPassword';
-import AdminDashboard from '../pages/dashboard/admin/AdminDashboard';
-import TeacherDashboard from '../pages/dashboard/teacher/TeacherDashboard';
-import StudentDashboard from '../pages/dashboard/student/StudentDashboard';
+
 
 export default function AppRoutes() {
   return (
@@ -16,9 +14,6 @@ export default function AppRoutes() {
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
-      <Route path="/student/dashboard" element={<StudentDashboard />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
